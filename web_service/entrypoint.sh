b08e1c7ab3a8479217ed196c29c0592603fec27d
bakhee <alkesey.gredyaev@gmail.com>
2024-04-24 18:17:37 +0300

    Add: superuser creation from env file

--- web_service/entrypoint.sh
+++ web_service/entrypoint.sh
@@ -11,6 +11,20 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Create superuser
+# check if superuser exists
+does_su_exist=$(python manage.py shell -c "from django.contrib.auth.models import User; \
+                           import os; \
+                           email = os.getenv('DJANGO_SUPERUSER_EMAIL'); \
+                           exists = User.objects.filter(email=email).exists(); \
+                           print(exists)")
+
+# create superuser if it doesn't exist
+if [ "$does_su_exist" == "False" ]; then
+  echo "Creating superuser..."
+  python manage.py createsuperuser --noinput
+fi
+
 # Start server
 echo "Starting server..."
 exec "$@"
