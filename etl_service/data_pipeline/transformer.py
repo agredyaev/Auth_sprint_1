0c29d7d69bd14f3fc551cf10e27095eac75a27a9
bakhee <alkesey.gredyaev@gmail.com>
2024-04-25 01:55:17 +0300

    Add: Transformer stage of the pipeline

--- etl_service/data_pipeline/transformer.py
+++ etl_service/data_pipeline/transformer.py
@@ -1,11 +1,37 @@
-class DataTransformer:
-    @staticmethod
-    def transform(records):
-        transformed_data = []
-        for record in records:
-            transformed_record = {
-                "es_id": record["id"],  # Example transformation
-                # Transform other fields as needed
-            }
-            transformed_data.append(transformed_record)
-        return transformed_data
+import datetime
+from typing import Callable, Generator
+
+from ..utility.logger import setup_logging
+from ..models.movies import Filmwork
+
+logger = setup_logging()
+
+
+class FilmworkTransformer:
+    def __init__(
+        self,
+        next_node: Callable[[], Generator[None, tuple[datetime.datetime, list[Filmwork]] | None, None]]
+    ):
+        self.next_node = next_node
+
+    def transform(self):
+        """
+        Implements the transformation process.
+        """
+        global last_updated
+        event_handler = self.next_node()
+        next(event_handler)
+
+        try:
+            while True:
+                last_updated, data_in = yield
+                rows: list[Filmwork]
+                for row in data_in:
+                    row.transform()
+
+                event_handler.send((last_updated, data_in))
+        except GeneratorExit:
+            logger.debug(
+                "Transformer finished with last_updated: %s",
+                last_updated
+            )
