d929f04c5cf7be9abdb3e358d05101fb7f9b3473
bakhee <alkesey.gredyaev@gmail.com>
2024-04-25 01:57:21 +0300

    Add: FilmworkExtractor class

---
+++ etl_service/data_pipeline/extractors/filmwork_extractor.py
@@ -0,0 +1,26 @@
+from base_extractor import BaseExtractor
+
+
+class FilmworkEtractor(BaseExtractor):
+    def __init__(self, pg_conn, state, batch_size, next_node):
+        super().__init__(pg_conn, state, batch_size, next_node)
+        self.proc_table = "film_work"
+
+    @property
+    def _enrich_data_query(self):
+        return None
+
+    def _enrich_data(self):
+        """
+        Overrides parent method
+        """
+        event_handler = self._merge()
+        next(event_handler)
+
+        try:
+            while True:
+                last_updated, rows = (yield)
+                event_handler.send((last_updated, rows))
+
+        except GeneratorExit:
+            pass
