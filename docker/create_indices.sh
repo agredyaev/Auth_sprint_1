b1d1447e19f8450515056d86608ba301748da718
bakhee <alkesey.gredyaev@gmail.com>
2024-05-13 21:09:26 +0300

    Update: already exist index handler

--- docker/create_indices.sh
+++ docker/create_indices.sh
@@ -5,9 +5,14 @@ set -x
 create_index() {
     local index_file=$1
     local index_name=$(basename "$index_file" .json)
-    echo "Creating index: $index_name"
-    curl -X PUT "$ES_DSN/$index_name" -H 'Content-Type: application/json' -d "@$index_file"
-    echo "Index $index_name created."
+
+    if curl -I -X HEAD "$ES_DSN/$index_name" | grep -q '200 OK'; then
+        echo "Index $index_name already exists. Shipping..."
+    else
+      echo "Creating index: $index_name"
+      curl -X PUT "$ES_DSN/$index_name" -H 'Content-Type: application/json' -d "@$index_file"
+      echo "Index $index_name created."
+    fi
 }
 
 for index_file in ./indices/*.json; do
