c6d98f7400bfbdf1718f8a33c026b30bd4b5d547
bakhee <alkesey.gredyaev@gmail.com>
2024-05-13 18:19:47 +0300

    Update: data model queries

--- etl_service/sql/movies.sql
+++ etl_service/sql/movies.sql
@@ -3,15 +3,27 @@ SELECT
     fw.rating as rating,
     fw.title,
     fw.description,
-    COALESCE (
-        json_agg(
-            DISTINCT jsonb_build_object(
-                'id', g.id,
-                'name', g.name
-            )
-        ) FILTER (WHERE g.id is not null),
-        '[]'
-    ) as genres,
+    fw.updated_at,
+    COALESCE(
+        json_agg(DISTINCT g.name),'[]') as genres,
+    COALESCE(
+       json_agg(
+           DISTINCT p.full_name
+       ) FILTER (WHERE p.id is not null AND pfw.role = 'director'),
+       '[]'
+    ) as directors_names,
+    COALESCE(
+       json_agg(
+           DISTINCT p.full_name
+       ) FILTER (WHERE p.id is not null AND pfw.role = 'actor'),
+       '[]'
+    ) as actors_names,
+    COALESCE(
+       json_agg(
+            DISTINCT p.full_name
+       ) FILTER (WHERE p.id is not null AND pfw.role = 'writer'),
+       '[]'
+    ) as writers_names,
     COALESCE (
         json_agg(
             DISTINCT jsonb_build_object(
@@ -46,6 +58,11 @@ LEFT JOIN content.person p ON p.id = pfw.person_id
 LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
 LEFT JOIN content.genre g ON g.id = gfw.genre_id
 WHERE
-    fw.id IN %s
+    fw.updated_at > %(updated_at)s
+    OR g.updated_at > %(updated_at)s
+    OR p.updated_at > %(updated_at)s
 GROUP BY
-    fw.id;
+    fw.id
+ORDER BY
+    fw.updated_at DESC
+ ;
---
+++ etl_service/sql/people.sql
@@ -0,0 +1,29 @@
+WITH film_work_roles as (
+    SELECT
+        fw.id,
+        fw.title,
+        array_agg(pfw.role) as roles,
+        pfw.person_id
+        FROM content.film_work fw
+        LEFT JOIN content.person_film_work pfw on fw.id = pfw.film_work_id
+        GROUP BY fw.id, pfw.person_id
+)
+SELECT
+    p.id,
+    p.full_name,
+    p.updated_at,
+    COALESCE(
+        json_agg(
+            DISTINCT jsonb_build_object(
+                'id', fr.id,
+                'title', fr.title,
+                'roles', fr.roles
+            )
+        ), '[]'
+    ) as movies
+FROM content.person p
+LEFT JOIN film_work_roles fwr on fwr.person_id = p.id
+WHERE p.updated_at > %(updated_at)s
+GROUP BY p.id
+ORDER BY p.updated_at DESC
+;
