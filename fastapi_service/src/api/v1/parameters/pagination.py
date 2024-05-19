ab25abfbc34295efa5698a2a062ea12bf1ca959c
bakhee <alkesey.gredyaev@gmail.com>
2024-05-20 00:11:14 +0300

    Add: parameters - pagination

---
+++ fastapi_service/src/api/v1/parameters/pagination.py
@@ -0,0 +1,15 @@
+from fastapi import Query
+from pydantic import BaseModel
+from fastapi_service.src.core.config import settings
+
+
+class PaginationParameters(BaseModel):
+    page: int
+    size: int
+
+
+def fetch_pagination_parameters(
+        page: int = Query(settings.api.default_page_number, gt=0, alias="page_number"),
+        size: int = Query(settings.api.default_page_size, gt=0, alias="page_size"),
+) -> PaginationParameters:
+    return PaginationParameters(page=page, size=size)
