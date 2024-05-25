a46b3bff976b48b3e253b7d077411a74252f598a
bakhee <alkesey.gredyaev@gmail.com>
2024-05-25 16:25:02 +0300

    Add: person tests

---
+++ tests/functional/src/test_person.py
@@ -0,0 +1,107 @@
+from http import HTTPStatus
+
+import pytest
+from faker import Faker
+
+from tests.functional.settings import config
+from tests.functional.testdata.person import STATIC_PERSON_FULL_NAME, STATIC_PERSON_ID, STATIC_PERSON_NUMBER_OF_FILMS
+
+fake = Faker()
+
+
+@pytest.mark.parametrize(
+    "query_data, expected_answer",
+    [
+        (
+            {
+                "query": STATIC_PERSON_FULL_NAME,
+            },
+            {"status": HTTPStatus.OK, "length": 1},
+        ),
+        (
+            {
+                "query": "Unknown_Name",
+            },
+            {"status": HTTPStatus.OK, "length": 0},
+        ),
+        (
+            {
+                "query": STATIC_PERSON_FULL_NAME[:-1] + "x",
+            },
+            {"status": HTTPStatus.OK, "length": 1},
+        ),
+        (
+            {
+                "query": STATIC_PERSON_FULL_NAME.split(" ")[0],
+            },
+            {"status": HTTPStatus.OK, "length": 4},
+        ),
+    ],
+)
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("prepare_persons_data")
+async def test_persons_search(make_get_request, query_data, expected_answer):
+    """
+    Persons search
+    """
+    url = config.infra.api.dsn + "/api/v1/persons/search"
+    body, _, status = await make_get_request(url, query_data)
+
+    assert status == expected_answer["status"]
+    assert len(body) == expected_answer["length"]
+
+
+@pytest.mark.parametrize(
+    "uuid, expected_status",
+    [
+        (STATIC_PERSON_ID, HTTPStatus.OK),
+    ],
+)
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("prepare_persons_data")
+async def test_person_detail(make_get_request, uuid, expected_status):
+    """
+    Person detail
+    """
+    url = config.infra.api.dsn + "/api/v1/persons/" + uuid
+    body, _, status = await make_get_request(url)
+
+    assert status == expected_status
+    assert body["uuid"] == uuid
+
+
+@pytest.mark.parametrize(
+    "uuid, expected_status",
+    [
+        (fake.uuid4(), HTTPStatus.NOT_FOUND),
+    ],
+)
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("prepare_persons_data")
+async def test_person_not_found(make_get_request, uuid, expected_status):
+    """
+    Person not found
+    """
+    url = config.infra.api.dsn + "/api/v1/persons/" + uuid
+    _, _, status = await make_get_request(url)
+
+    assert status == expected_status
+
+
+@pytest.mark.parametrize(
+    "uuid, expected_status",
+    [
+        (STATIC_PERSON_ID, HTTPStatus.OK),
+    ],
+)
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("prepare_persons_data")
+async def test_persons_films(make_get_request, uuid, expected_status):
+    """
+    Person films
+    """
+    url = config.infra.api.dsn + f"/api/v1/persons/{uuid}/films"
+    body, _, status = await make_get_request(url)
+
+    assert status == expected_status
+    assert len(body) == STATIC_PERSON_NUMBER_OF_FILMS
