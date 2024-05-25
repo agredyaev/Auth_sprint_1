8a75a11164af776b52c10e61b301b333fb5fe447
bakhee <alkesey.gredyaev@gmail.com>
2024-05-25 16:18:28 +0300

    Add: infrastructure fixtures

---
+++ tests/functional/src/fixtures/elastic.py
@@ -0,0 +1,15 @@
+import pytest_asyncio
+from elasticsearch import AsyncElasticsearch
+
+from tests.functional.settings import config
+
+
+@pytest_asyncio.fixture(scope="session", name="es_client")
+async def es_client():
+    async with AsyncElasticsearch(hosts=[config.infra.es.dsn]) as client:
+        yield client
+
+
+@pytest_asyncio.fixture(scope="session", name="clear_es_data")
+async def clear_es_data(es_client: AsyncElasticsearch):
+    await es_client.indices.delete(index="_all")
