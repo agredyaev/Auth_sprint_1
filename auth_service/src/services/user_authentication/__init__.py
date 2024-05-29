aebd413a88e58678b32ff14830a0c5f08d4c5002
bakhee <alkesey.gredyaev@gmail.com>
2024-05-29 17:40:59 +0300

    Add: auth service structure

---
+++ auth_service/src/services/user_authentication/__init__.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+from fastapi import Depends
+
+from auth_service.src.services.user_repository import get_user_repository, UserRepositoryProtocol
+from auth_service.src.services.user_authentication.service import UserAuthenticationService, UserAuthenticationProtocol
+
+
+@lru_cache()
+def get_user_authentication_service(
+    user_repo: UserRepositoryProtocol = Depends(get_user_repository)
+) -> UserAuthenticationProtocol:
+    """
+    Provider for UserAuthenticationService
+    """
+    return UserAuthenticationService(user_repo)
