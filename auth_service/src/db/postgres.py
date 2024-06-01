66974bba16f4d7d05e53ac0c1a147240b4598254
bakhee <alkesey.gredyaev@gmail.com>
2024-06-01 13:27:03 +0300

    Update: db postgres methods to handle exceptions correctly

--- auth_service/src/db/postgres.py
+++ auth_service/src/db/postgres.py
@@ -2,24 +2,32 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from auth_service.src.core.config import settings
 
+
 Base = declarative_base()
 
-DATABASE_URL = settings.DATABASE_URL
-
-engine = create_async_engine(settings.pg.dsn, echo=True, future=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-
-async def get_session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
+engine = create_async_engine(url=settings.pg.dsn, echo=settings.pg.echo_sql_queries, future=True)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def create_database() -> None:
+    """Create database if it doesn't exist"""
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
 async def purge_database() -> None:
+    """Drop all tables in the database"""
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
+
+async def get_session() -> AsyncSession:
+    """Get a session for database operations"""
+    async with async_session() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
