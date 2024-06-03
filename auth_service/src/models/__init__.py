393451a8b1ec41a3a06e41a45043a391d418dc09
bakhee <alkesey.gredyaev@gmail.com>
2024-06-03 15:50:29 +0300

    Add: orm models import to init file

---
+++ auth_service/src/models/__init__.py
@@ -0,0 +1,11 @@
+from auth_service.src.models.user import User, UserRole
+from auth_service.src.models.role import Role
+from auth_service.src.models.login_history import LoginHistory
+
+
+__all__ = [
+    "User",
+    "UserRole",
+    "Role",
+    "LoginHistory",
+]
