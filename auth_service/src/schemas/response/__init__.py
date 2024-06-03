6d6d7a61ea95c449997b46d862001de2c78b7148
bakhee <alkesey.gredyaev@gmail.com>
2024-06-03 16:27:09 +0300

    Add: structure schemas response

---
+++ auth_service/src/schemas/response/__init__.py
@@ -0,0 +1,10 @@
+from role import *
+from user import UserCreate, UserResponse, UserUpdate, LoginHistory
+
+
+__all__ = [
+    "LoginHistory",
+    "UserCreate",
+    "UserResponse",
+    "UserUpdate",
+]
