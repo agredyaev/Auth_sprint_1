69eafd6a3d70c48e3a5d27aef2c81bc90d876b35
bakhee <alkesey.gredyaev@gmail.com>
2024-06-04 05:11:06 +0300

    Refactor: login history orm model (#8)

--- auth_service/src/models/login_history.py
+++ auth_service/src/models/login_history.py
@@ -1,14 +1,12 @@
-from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from auth_service.src.models.base import Base
-from auth_service.src.models.mixins import DataType, IdMixin, LoginTimeMixin, LogoutTimeMixin, UserIdMixin, \
-    CreatedAtMixin
 from auth_service.src.core.config import settings
+from auth_service.src.models.base import Base, STR_255
+from auth_service.src.models.mixins import CreatedAtMixin, IdMixin, LoginTimeMixin, LogoutTimeMixin, UserIdMixin
 
 
-class LoginHistory(Base, IdMixin, UserIdMixin, LoginTimeMixin, LogoutTimeMixin, CreatedAtMixin):
+class LoginHistory(Base, CreatedAtMixin, IdMixin, LoginTimeMixin, LogoutTimeMixin, UserIdMixin):
     __tablename__ = "login_history"
     __table_args__ = {"schema": settings.pg.db_schema}
 
-    user_agent: Mapped[DataType.STR_255] = mapped_column(String(255), nullable=False)
+    user_agent: Mapped[STR_255] = mapped_column(nullable=False)
