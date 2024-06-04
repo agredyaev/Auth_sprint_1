ad6a92d417623b6222e19a8f0d523a667ee19f39
bakhee <alkesey.gredyaev@gmail.com>
2024-06-04 05:09:03 +0300

    Refactor: role orm model (#8)

--- auth_service/src/models/role.py
+++ auth_service/src/models/role.py
@@ -1,13 +1,12 @@
-from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from auth_service.src.core.config import settings
-from auth_service.src.models.base import Base
-from auth_service.src.models.mixins import DataType, IdMixin, NameMixin, CreatedAtMixin, UpdatedAtMixin
+from auth_service.src.models.base import STR_255, Base
+from auth_service.src.models.mixins import CreatedAtMixin, IdMixin, NameMixin, UpdatedAtMixin
 
 
 class Role(Base, IdMixin, NameMixin, CreatedAtMixin, UpdatedAtMixin):
-    __tablename__ = "roles"
-    _table_args__ = {"schema": settings.pg.db_schema}
+    __tablename__ = "role"
+    __table_args__ = {"schema": settings.pg.db_schema}
 
-    description: Mapped[DataType.STR_255] = mapped_column(String(255), nullable=True)
+    description: Mapped[STR_255] = mapped_column(nullable=True)
