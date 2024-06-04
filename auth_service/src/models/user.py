a9dfcc0131c5ffbeb31b3e61835ca333c62073f1
bakhee <alkesey.gredyaev@gmail.com>
2024-06-04 05:08:51 +0300

    Refactor: user orm model (#8)

--- auth_service/src/models/user.py
+++ auth_service/src/models/user.py
@@ -1,23 +1,25 @@
-from sqlalchemy import UUID, ForeignKey, String
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from auth_service.src.core.config import settings
-from auth_service.src.models.base import Base
-from auth_service.src.models.mixins import IdMixin, CreatedAtMixin, UserIdMixin, DataType, UpdatedAtMixin
+from auth_service.src.models.base import STR_50, STR_255, STR_512, Base
+from auth_service.src.models.mixins import CreatedAtMixin, IdMixin, UpdatedAtMixin, UserIdMixin
 
 
 class User(Base, IdMixin, CreatedAtMixin, UpdatedAtMixin):
-    __tablename__ = "users"
-    _table_args__ = {"schema": settings.pg.db_schema}
+    __tablename__ = "user"
+    __table_args__ = {"schema": settings.pg.db_schema}
 
-    login: Mapped[DataType.STR_255] = mapped_column(String(255), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(255), nullable=False)
-    first_name: Mapped[DataType.STR_50] = mapped_column(String(50), nullable=True)
-    last_name: Mapped[DataType.STR_50] = mapped_column(String(50), nullable=True)
+    login: Mapped[STR_255] = mapped_column(unique=True, nullable=False)
+    password: Mapped[STR_512] = mapped_column(nullable=False)
+    first_name: Mapped[STR_50] = mapped_column(nullable=True)
+    last_name: Mapped[STR_50] = mapped_column(nullable=True)
 
 
 class UserRole(Base, UserIdMixin, CreatedAtMixin):
-    __tablename__ = "user_roles"
-    _table_args__ = {"schema": settings.pg.db_schema}
+    __tablename__ = "user_role"
+    __table_args__ = {"schema": settings.pg.db_schema}
 
-    role_id: Mapped[UUID] = mapped_column(ForeignKey(column="roles.id"), nullable=False)
+    role_id: Mapped[UUID] = mapped_column(ForeignKey(column=f"{settings.pg.db_schema}.role.id"), nullable=False)
