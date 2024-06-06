0d379008bcb0aa2ad311e92905f0c5ea27027c79
bakhee <alkesey.gredyaev@gmail.com>
2024-06-06 22:46:59 +0300

    Add: unique constaint to user role

--- auth_service/src/models/user_role.py
+++ auth_service/src/models/user_role.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from auth_service.src.core.config import settings
@@ -10,8 +10,10 @@ from auth_service.src.models.mixins import CreatedAtMixin, IdMixin, UserIdMixin
 
 class UserRole(Base, IdMixin, UserIdMixin, CreatedAtMixin):
     __tablename__ = "user_role"
-    __table_args__ = {"schema": settings.pg.db_schema}
-
+    __table_args__ = (
+        UniqueConstraint('user_id', 'role_id', name='unique_user_role'),
+        {"schema": settings.pg.db_schema},
+    )
     role_id: Mapped[UUID] = mapped_column(
         ForeignKey(column=f"{settings.pg.db_schema}.role.id", ondelete="CASCADE"), nullable=False
     )
