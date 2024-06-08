b4eb21bd0018a1906e71e99b0add714669ebf900
bakhee <alkesey.gredyaev@gmail.com>
2024-06-09 02:04:10 +0300

    Update: Token schemas (#14) (#20)

--- auth_service/src/schemas/request/token.py
+++ auth_service/src/schemas/request/token.py
@@ -1,13 +1,48 @@
-from auth_service.src.schemas.mixins import AccessTokenMixin, RefreshTokenMixin
+from pydantic import BaseModel, Field, field_validator
+
+from auth_service.src.core.config import settings as conf
+from auth_service.src.schemas.mixins import AccessTokenMixin, RefreshTokenMixin, UserAgentMixin, UserNameMixin
 
 
-class Token(AccessTokenMixin, RefreshTokenMixin):
-    token_type: str
+class Token(BaseModel):
+    name: str = Field(default="token", description="jti")
+    time: int = Field(default=0, description="exp")
+    value: str = Field(default="false", description="is_in_denylist")
+
+    @field_validator("name", mode="before")
+    def set_name(cls, v: str) -> str:
+        return f"{conf.redis.namespace}:{v}"
 
 
-class TokenRefreshRequest(RefreshTokenMixin):
-    ...
+class TokenRefreshStore(Token):
+    time: int = conf.jwt.refresh_expires
 
 
-class LogoutRequest(AccessTokenMixin):
-    ...
+class TokenRefreshToDenylist(TokenRefreshStore):
+    value: str = "true"
+
+
+class TokenAccessToDenylist(Token):
+    time: int = conf.jwt.access_expires
+    value: str = "true"
+
+
+class TokenGet(Token):
+    pass
+
+
+class TokensCreate(UserNameMixin, UserAgentMixin):
+    pass
+
+
+class Login(UserNameMixin):
+    username: str
+    password: str
+
+
+class Logout(AccessTokenMixin):
+    pass
+
+
+class TokenRefreshData(RefreshTokenMixin, UserAgentMixin):
+    pass
