402a608a33ed4358c797f96455cc223874a33165
bakhee <alkesey.gredyaev@gmail.com>
2024-06-18 04:15:50 +0300

    Add: implementation of users endpoint (#47)

--- auth_service/src/api/__init__.py
+++ auth_service/src/api/__init__.py
@@ -1,10 +1,6 @@
 from fastapi.routing import APIRouter
 
+from auth_service.src.api.v1 import router as v1_router
 
-# from auth_service.src.api.v1 import router as v1_router
-from auth_service.src.api.healthcheck import healthcheck_router
-
-#
 router = APIRouter()
-# router.include_router(v1_router, prefix="/v1")
-router.include_router(healthcheck_router)
+router.include_router(v1_router, prefix="/v1")
--- auth_service/src/api/v1/__init__.py
+++ auth_service/src/api/v1/__init__.py
@@ -1,9 +1,10 @@
-# from fastapi.routing import APIRouter
-#
-# from auth_service.src.api.v1.auth import auth_router
-# from auth_service.src.api.v1.role import role_router
-#
-#
-# router = APIRouter()
-# router.include_router(auth_router)
-# router.include_router(role_router)
+from fastapi.routing import APIRouter
+
+from auth_service.src.api.v1.healthcheck import router as healthcheck_router
+from auth_service.src.api.v1.roles import router as roles_router
+from auth_service.src.api.v1.users import router as users_router
+
+router = APIRouter()
+router.include_router(users_router)
+router.include_router(roles_router)
+router.include_router(healthcheck_router)
