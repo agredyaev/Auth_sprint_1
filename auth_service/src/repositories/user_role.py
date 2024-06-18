cbcb42826800cc8871fbf4af53a500a38f23b526
bakhee <alkesey.gredyaev@gmail.com>
2024-06-18 04:26:32 +0300

    Add: User-Role repository protocol implementation and request-response schemas (#48)

--- auth_service/src/repositories/user_role.py
+++ auth_service/src/repositories/user_role.py
@@ -1,15 +1,21 @@
-from typing import Type
-from uuid import UUID
+from typing import Sequence, Type
 
-from sqlalchemy import select
+from sqlalchemy import and_, delete, insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth_service.src.core.logger import setup_logging
 from auth_service.src.interfaces.repositories import UserRoleRepositoryProtocol
-from auth_service.src.models import UserRole
-from auth_service.src.repositories import PostgresRepository
-from auth_service.src.schemas.request import UserRoleAssign, UserRoleRevoke
-from auth_service.src.utils.db_operations import execute_single_query
+from auth_service.src.models import Role, User, UserRole
+from auth_service.src.repositories.postgres import PostgresRepository
+from auth_service.src.schemas.request import (
+    UserGetRolePermissions,
+    UserRoleAssign,
+    UserRoleCreate,
+    UserRoleRevoke,
+    UserRoleVerify,
+)
+from auth_service.src.utils.combinations import create_combinations
+from auth_service.src.utils.db_operations import execute_list_query
 
 logger = setup_logging(__name__)
 
@@ -23,17 +29,36 @@ class UserRoleRepository(UserRoleRepositoryProtocol[UserRole], PostgresRepositor
         super().__init__(db_session=db_session)
         self.db_session = db_session
 
-    async def assign_role(self, user_data: UserRoleAssign) -> None:
-        return await self.create(user_data)
+    async def create_records(self, user_data: UserRoleAssign) -> Sequence[UserRole]:
+        values = create_combinations(user_data, UserRoleCreate)
+        query = insert(self._model).values(values).returning(self._model)
+        user_roles: Sequence[UserRole] = await execute_list_query(self.db_session, query)
 
-    async def revoke_role(self, user_data: UserRoleRevoke) -> None:
-        query = select(self._model).filter(
-            self._model.user_id == user_data.user_id, self._model.role_id == user_data.role_id
+        return user_roles
+
+    async def delete_records(self, user_data: UserRoleRevoke) -> Sequence[UserRole]:
+        roles = [role.role_id for role in user_data.roles]
+        condition = and_(self._model.user_id == user_data.user_id, self._model.role_id.in_(roles))
+        delete_query = delete(self._model).where(condition).returning(self._model)
+        user_roles: Sequence[UserRole] = await execute_list_query(self.db_session, delete_query)
+
+        return user_roles
+
+    async def get_records(self, user_data: UserGetRolePermissions) -> Sequence[UserRole]:
+        query = select(self._model).where(self._model.user_id == user_data.user_id)
+        user_roles: Sequence[UserRole] = await execute_list_query(self.db_session, query)
+
+        return user_roles
+
+    async def check_records(self, user_data: UserRoleVerify) -> Sequence[UserRole]:
+        query = (
+            select(self._model)
+            .join(User, self._model.user_id == User.id)
+            .join(Role, self._model.role_id == Role.id)
+            .where(User.email == user_data.email)
+            .where(Role.name.in_([role.name for role in user_data.role_names]))
         )
-        user_role_id: UUID | None = await execute_single_query(self.db_session, query)
-        if user_role_id is not None:
-            return await self.delete(user_role_id)
-        else:
-            logger.error(
-                f"Role assignment not found for the given user {user_data.user_id} and role {user_data.role_id} IDs."
-            )
+
+        user_roles: Sequence[UserRole] = await execute_list_query(self.db_session, query)
+
+        return user_roles
