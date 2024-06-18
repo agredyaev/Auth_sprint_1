3bc5335a64c896777cd393bcea9e4104f8f9cffb
bakhee <alkesey.gredyaev@gmail.com>
2024-06-18 04:42:35 +0300

    Refactor: role repository protocol implementation  (#19)

--- auth_service/src/interfaces/repositories/role.py
+++ auth_service/src/interfaces/repositories/role.py
@@ -8,16 +8,16 @@ P = TypeVar("P", bound=Any)
 
 
 class RoleRepositoryProtocol(PostgresRepositoryProtocol[T]):
-    async def create_role(self, role_data: P) -> None:
+    async def create_role(self, role_data: P) -> T:
         raise NotImplementedError
 
     async def get_role(self, role_id: UUID) -> T | None:
         raise NotImplementedError
 
-    async def update_role(self, role_data: P) -> None:
+    async def update_role(self, role_data: P) -> T:
         raise NotImplementedError
 
-    async def delete_role(self, role_id: UUID) -> None:
+    async def delete_role(self, role_id: UUID) -> T:
         raise NotImplementedError
 
     async def list_roles(self) -> Sequence[T]:
